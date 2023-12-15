0f22e63b8dba6f968c6b79c1357de11cbebf3a06
linirini <2001yerin@naver.com>
2023-12-15 21:56:03 +0900

    feat : add exception messages

--- src/main/java/menu/util/ExceptionEnum.java
+++ src/main/java/menu/util/ExceptionEnum.java
@@ -4,7 +4,8 @@ public enum ExceptionEnum {
 
     INVALID_NAME_LENGTH("코치 이름은 2글자 이상, 4글자 미만이어야 합니다."),
     INVALID_MENU_COUNT("못 먹는 메뉴는 0개 이상, 2개 이하만 입력 가능합니다."),
-    DUPLICATED_MENU("중복되는 메뉴를 입력하였습니다.");
+    DUPLICATED_MENU("중복되는 메뉴를 입력하였습니다."),
+    INVALID_NAME_COUNT("코치는 2명이상, 5명 이하까지 입력할 수 있습니다.");
 
     private static final String ERROR = "[ERROR] ";
     private String message;
