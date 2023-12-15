519c3a520595b9e1887695b2ea5846841623c66e
linirini <2001yerin@naver.com>
2023-12-15 18:58:08 +0900

    feat : implement input views

---
+++ src/main/java/menu/view/InputView.java
@@ -0,0 +1,17 @@
+package menu.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+
+    public String inputCoachNames(){
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+        return readLine();
+    }
+
+    public String inputCannotEatMenus(Coach coach){
+        System.out.println(coach.getName()+"(이)가 못 먹는 메뉴를 입력해 주세요.");
+        return readLine();
+    }
+
+}
