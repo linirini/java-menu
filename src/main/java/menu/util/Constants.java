b1de20eb3009e576ced9b7753e982b9d8f4bb229
linirini <2001yerin@naver.com>
2023-12-15 20:08:46 +0900

    feat : implement printing recommendation result

--- src/main/java/menu/util/Constants.java
+++ src/main/java/menu/util/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static final String CHINESE_NUMBER ="3";
     public static final String ASIAN_NUMBER="4";
     public static final String WESTERN_NUMBER = "5";
+
 }
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,8 +1,17 @@
 package menu.view;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+import menu.domain.Coach;
+import menu.domain.Menu;
 import menu.domain.RecommendResult;
 
 public class OutputView {
+
+    public static final String OPEN_SQUARE_BRACKET = "[ ";
+    public static final String CLOSE_SQUARE_BRACKET = " ]";
+    public static final String MENU_SEPARATOR = " | ";
     
     public void printStartMessage(){
         System.out.println("점심 메뉴 추천을 시작합니다.");
@@ -17,6 +26,20 @@ public class OutputView {
     }
 
     private void printRecommendResult(RecommendResult recommendResult) {
+        for (Entry<Coach, List<Menu>> entry : recommendResult.getResults().entrySet()) {
+            StringBuilder output = new StringBuilder();
+            output.append(OPEN_SQUARE_BRACKET);
+            output.append(entry.getKey().getName()+MENU_SEPARATOR);
+            output.append(String.join(MENU_SEPARATOR,getMenuNames(entry.getValue())));
+            output.append(CLOSE_SQUARE_BRACKET);
+            System.out.println(output);
+        }
+    }
+
+    private List<String> getMenuNames(List<Menu> menus) {
+        List<String> names = new ArrayList<>();
+        menus.forEach(menu -> names.add(menu.getName()));
+        return names;
     }
 
 }
